71954873366f39a38a1fd19386e64bc6288b04a3
Barklim <46284548+Barklim@users.noreply.github.com>
2021-01-14 21:08:21 +0300

    add redis

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,35 +2,17 @@ import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
-// import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { User } from "./entities/User";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
-  // dbName: "a1111",
-  // password: "web13",
-  // type: "postgresql",
-  //     // name: "a1111",
-  // user: "a1111",
-
-  // dbName: "a1111",
-  // // password: "web13",
-  // // type: "postgresql",
-  //     name: "a1111",
-  // user: "a1111",
-
+  entities: [Post, User],
   dbName: "lireddit",
   type: "postgresql",
   // driver: PostgreSqlDriver,
   password: "web13",
-
-  // dbName: "creeperland",
-  // password: "web13",
-  // type: "postgresql",
-  //     // name: "postgres",
-  // user: "postgres",
   debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
