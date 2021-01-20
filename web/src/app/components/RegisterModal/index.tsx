c9f46cd6a958cec43432857eaf4efb0ed2606763
Barklim <46284548+Barklim@users.noreply.github.com>
2021-01-20 20:21:45 +0300

    testing react bootsrap animations

--- web/src/app/components/RegisterModal/index.tsx
+++ web/src/app/components/RegisterModal/index.tsx
@@ -1,17 +1,11 @@
-/**
-*
-* RegisterModal
-*
-*/
 import * as React from 'react';
 import styled from 'styled-components/macro';
 import { useTranslation } from 'react-i18next';
-import { messages } from './messages';
-// const { Formik } = formik;
-import { Formik } from "formik";
+// import { messages } from './messages';
+import { Formik } from 'formik';
 import * as yup from 'yup';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+// import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
@@ -33,143 +27,140 @@ export function RegisterModal(props: Props) {
   });
 
   return (
-  <Div>
-    {t('')}
-    {/*  {t(...messages.someThing)}  */}
-
-    <Formik
-      validationSchema={schema}
-      onSubmit={console.log}
-      initialValues={{
-        firstName: 'Mark',
-        lastName: 'Otto',
-        username: 'username',
-        city: 'city',
-        state: 'state',
-        zip: 'zip',
-        terms: 'terms'
-      }}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        values,
-        touched,
-        isValid,
-        errors,
-      }) => (
-        <Form noValidate onSubmit={handleSubmit}>
-          <Form.Row>
-            <Form.Group as={Col} md="4" controlId="validationFormik01">
-              <Form.Label>First name</Form.Label>
-              <Form.Control
-                type="text"
-                name="firstName"
-                value={values.firstName}
-                onChange={handleChange}
-                isValid={touched.firstName && !errors.firstName}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormik02">
-              <Form.Label>Last name</Form.Label>
-              <Form.Control
-                type="text"
-                name="lastName"
-                value={values.lastName}
-                onChange={handleChange}
-                isValid={touched.lastName && !errors.lastName}
-              />
-
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormikUsername">
-              <Form.Label>Username</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                </InputGroup.Prepend>
+    <Div>
+      {t('')}
+      {/*  {t(...messages.someThing)}  */}
+      <Formik
+        validationSchema={schema}
+        onSubmit={console.log}
+        initialValues={{
+          firstName: 'Mark',
+          lastName: 'Otto',
+          username: 'username',
+          city: 'city',
+          state: 'state',
+          zip: 'zip',
+          terms: 'terms',
+        }}
+      >
+        {({
+          handleSubmit,
+          handleChange,
+          handleBlur,
+          values,
+          touched,
+          isValid,
+          errors,
+        }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Form.Row>
+              <Form.Group as={Col} md="4" controlId="validationFormik01">
+                <Form.Label>First name</Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder="Username"
-                  aria-describedby="inputGroupPrepend"
-                  name="username"
-                  value={values.username}
+                  name="firstName"
+                  value={values.firstName}
                   onChange={handleChange}
-                  isInvalid={!!errors.username}
+                  isValid={touched.firstName && !errors.firstName}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationFormik02">
+                <Form.Label>Last name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="lastName"
+                  value={values.lastName}
+                  onChange={handleChange}
+                  isValid={touched.lastName && !errors.lastName}
+                />
+
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationFormikUsername">
+                <Form.Label>Username</Form.Label>
+                <InputGroup>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <Form.Control
+                    type="text"
+                    placeholder="Username"
+                    aria-describedby="inputGroupPrepend"
+                    name="username"
+                    value={values.username}
+                    onChange={handleChange}
+                    isInvalid={!!errors.username}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.username}
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} md="6" controlId="validationFormik03">
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="City"
+                  name="city"
+                  value={values.city}
+                  onChange={handleChange}
+                  isInvalid={!!errors.city}
+                />
+
+                <Form.Control.Feedback type="invalid">
+                  {errors.city}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="3" controlId="validationFormik04">
+                <Form.Label>State</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="State"
+                  name="state"
+                  value={values.state}
+                  onChange={handleChange}
+                  isInvalid={!!errors.state}
                 />
                 <Form.Control.Feedback type="invalid">
-                  {errors.username}
+                  {errors.state}
                 </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </Form.Row>
-          <Form.Row>
-            <Form.Group as={Col} md="6" controlId="validationFormik03">
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="City"
-                name="city"
-                value={values.city}
-                onChange={handleChange}
-                isInvalid={!!errors.city}
-              />
+              </Form.Group>
+              <Form.Group as={Col} md="3" controlId="validationFormik05">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Zip"
+                  name="zip"
+                  value={values.zip}
+                  onChange={handleChange}
+                  isInvalid={!!errors.zip}
+                />
 
-              <Form.Control.Feedback type="invalid">
-                {errors.city}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationFormik04">
-              <Form.Label>State</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="State"
-                name="state"
-                value={values.state}
+                <Form.Control.Feedback type="invalid">
+                  {errors.zip}
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Group>
+              <Form.Check
+                required
+                name="terms"
+                label="Agree to terms and conditions"
                 onChange={handleChange}
-                isInvalid={!!errors.state}
+                isInvalid={!!errors.terms}
+                feedback={errors.terms}
+                id="validationFormik0"
               />
-              <Form.Control.Feedback type="invalid">
-                {errors.state}
-              </Form.Control.Feedback>
             </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationFormik05">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Zip"
-                name="zip"
-                value={values.zip}
-                onChange={handleChange}
-                isInvalid={!!errors.zip}
-              />
-
-              <Form.Control.Feedback type="invalid">
-                {errors.zip}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Form.Row>
-          <Form.Group>
-            <Form.Check
-              required
-              name="terms"
-              label="Agree to terms and conditions"
-              onChange={handleChange}
-              isInvalid={!!errors.terms}
-              feedback={errors.terms}
-              id="validationFormik0"
-            />
-          </Form.Group>
-          <Button type="submit">Submit form</Button>
-        </Form>
-      )}
-    </Formik>
-    
-  </Div>
+            <Button type="submit">Submit form</Button>
+          </Form>
+        )}
+      </Formik>
+    </Div>
   );
-
-};
+}
 
 const Div = styled.div``;
